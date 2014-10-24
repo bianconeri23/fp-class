2759c19b359ad04b30110d4ba34c8e9cc7704716
bianconeri23 <chalt-vlad@yandex.ru>
2014-10-24 17:30:01 +0400

    6

--- class-06/02-textproc.hs
+++ class-06/02-textproc.hs
@@ -12,5 +12,59 @@
   Все входные данные программы должны передаваться с помощью параметров
   командной строки.
 -}
+import System.Environment
+import System.IO
+import Data.Char
+import System.Random
+import Data.List.Split
 
-main = undefined
+strCount f = do
+	handle <- openFile f ReadMode
+	contents <- hGetContents handle	
+	print (length(lines contents))
+	hClose handle
+
+insStart f s = do
+	contents <- readFile f
+    	length contents `seq` (writeFile f $ s++ contents)
+
+insEnd f s = do
+	appendFile f s
+	--contents <- readFile f
+    	--length contents `seq` (writeFile f $ contents ++ s)
+
+toUppFile f = do
+	contents <- readFile f
+	putStr (map toUpper contents)
+
+mergeFiles f1 f2 = do
+	contents<-readFile f1
+	contents2<-readFile f2
+	let r =myMerge (lines contents) (lines contents2)
+	putStr (r)
+	
+myMerge (x:xs)(y:ys) = x++y++(myMerge xs ys)
+
+doRandStrs cstr csymb gen = do
+						take cstr $ splitEvery csymb $ randomRs ('a','z') gen
+
+writeToFile f (x:[]) = appendFile f x
+writeToFile f (x:xs) = do 
+					appendFile f (x++['\n'])
+					writeToFile f xs 
+
+randomFile f cstr csymb =  do	
+						gen <- newStdGen
+						writeFile f "leeeets get it started \n"
+						let list = doRandStrs cstr csymb gen
+						writeToFile f list
+main = do 
+	[fileT1, fileT2,strT2, fileT3,file1T4,file2T4,fileT5,strMax,symbMax] <- getArgs
+	strCount fileT1
+	insStart fileT2 strT2
+	insEnd fileT2 strT2
+	toUppFile fileT3
+	mergeFiles file1T4 file2T4
+	let str=read (strMax):: Int
+	let symb= read (symbMax) :: Int
+	randomFile fileT5 str symb
--- class-06/06-bulls_n_cows.hs
+++ class-06/06-bulls_n_cows.hs
@@ -2,5 +2,37 @@
    Запрограммируйте игру «Быки и коровы» (https://ru.wikipedia.org/wiki/Быки_и_коровы)
    в варианте «компьютер загадывает — пользователь отгадывает».
 -}
+import System.Environment
+import System.IO
+import Data.Char
+import System.Random
+import Data.List.Split
+import Data.List
 
-main = undefined
+randN gen = do
+					take 4 $ randomRs (1,9) gen ::[Integer]
+
+playGame = do 
+					gen <- newStdGen
+					let list = randN gen
+					if (length $ group $ sort list) /=4 then playGame else doGame list
+snd'(x:xx:xs)=xx
+snd'(x:[])=if head x == 1 then x else []
+					
+checkBuls x x2 = length $ snd' $ group $ sort  $ snd $ mapAccumL(\xx y-> if ((!!)x xx) == y then (xx+1,1) else(xx+1,0) ) 0 x2
+checkCows x x2 = length $ snd' $ group $ sort  $ map(\xx-> if elem xx x then 1 else 0) x2	
+
+doGame x = do 
+					print "Enter your Num"
+					s<-getLine
+					let x2 = (map (\x-> fromIntegral $ digitToInt x) s)
+					let cows = checkCows x x2
+					let buls = checkBuls x x2
+					print "Cows"
+					print (cows-buls)
+					print "Buls"
+					print buls
+					if buls ==4 then print("You Won, gg wp , the number is " ++ s) else doGame x
+					
+
+main = playGame
