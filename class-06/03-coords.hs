910e2528f652f84cb5ad51572831cc69d40e31d8
bianconeri23 <chalt-vlad@yandex.ru>
2014-10-24 17:37:22 +0400

    6

--- class-06/03-coords.hs
+++ class-06/03-coords.hs
@@ -6,5 +6,51 @@
      из четвертей;
   в) отыскивает наиболее удалённую от начала координат точку.
 -}
+import System.Environment
+import System.IO
+import Data.Char
+import System.Random
+import Data.List.Split
+import Data.List
+doRandCord gen = do
+					take 100 $ randomRs (-100,100) gen
+					
+writeToFile f (x:xx:[]) = do 
+							appendFile f (show(x :: Int)++[' ']++show(xx :: Int)++['\n'])
+							--appendFile f (show(xx)++['\n'])
+writeToFile f (x:xx:xs) = do 
+					appendFile f (show(x)++[' ']++show(xx)++['\n'])
+					writeToFile f xs 
 
+
+generateFileCoord f = do	
+					gen <- newStdGen
+					let list = doRandCord gen
+					writeFile f ""						
+					writeToFile f list
+					
+watqart' (x,y)
+		|x>0 && y>0 = 1 
+		|x>0 && y<0 = 4 
+		|x<0 && y>0 = 2  
+		|otherwise = 3	
+		
+fst' (x:xs)=x
+snd' (x:xx:xs)=xx	
+occurencies = map (\xs -> (head xs, length xs)) . group . sort
+
+	
+howMuch([])=[]	
+howMuch (x:xs) = do 
+			let x1=splitOn " " x
+			watqart' (read(fst' x1),read(snd' x1)):howMuch xs
+			
+countOfCoords f=do
+			contents <-readFile f
+			let list = lines contents
+			let list2 = howMuch list
+			let list3 = occurencies list2
+			print (list3)
+			
+			
 main = undefined
--- class-06/04-db.hs
+++ class-06/04-db.hs
@@ -12,5 +12,67 @@
         ФАМИЛИЯ И.О.
 
 -}
+import Data.List
+import Data.List.Split
+import System.Environment
+import System.IO
 
-main = undefined
+			
+avgAge2 xx = read (snd' $ splitOn ";" xx) ::Integer
+
+avgAge([])=0
+avgAge(x:xs)=avgAge2 x + avgAge xs
+	
+fst' (x:xs) = x
+snd' (x:[]) = []		
+snd' (x:xx:xs) = xx
+thr (x:[])=[]
+thr (x:xx:xxx:xs)=xxx
+fr (x:xx:xxx:xxxx:xs)=xxxx
+				
+onlyCoGr co gr xx = do 
+					let x = splitOn ";" xx
+					read(thr x) == co && read(fr x)==gr
+
+
+studAvgAge f co gr = do 
+			contents <-readFile f
+			let list=lines contents
+			let list2 = filter (onlyCoGr co gr) list
+			let m = avgAge list2
+			print m
+
+fCourseG x y = (read (thr $ splitOn ";" x) ::Integer) == (read (thr $ splitOn ";" y) :: Integer)
+
+
+fCourseS x y  
+			| (read(thr $ splitOn ";" x)::Integer) < (read(thr $ splitOn ";" y)::Integer) =LT
+			| otherwise =GT
+fGroupG x y = (read (fr $ splitOn ";" x) ::Integer) == (read (fr $ splitOn ";" y) :: Integer)
+
+occurencies= map (\xs -> groupBy (fGroupG) xs) . groupBy (fCourseG).sortBy (fCourseS)
+
+countStud f = do
+				contents <-readFile f
+				let list=lines contents
+				let list2 = occurencies list
+				let list3 =map(\xs->map(\xs->(thr $ splitOn ";"$ head xs,fr $ splitOn ";" $ head xs, length xs))xs) list2
+				print "Stud Count "
+				print list3		
+
+studStr xs = map(\x->(fst' $ splitOn " " $ fst' $ splitOn ";" x) ++" " ++ [(fst' $ snd' $ splitOn " " $ fst' $ splitOn ";" x)]++". " ++ [(fst' $ thr $ splitOn " " $ fst' $ splitOn ";" x)]++"." )xs
+
+allStud f = do 
+				contents <-readFile f
+				let list=lines contents
+				let list2 = occurencies list
+				mapM(\xs->mapM(\xs->appendFile ((thr $ splitOn ";"$ head xs)++"_"++(fr $ splitOn ";" $ head xs)++".txt") (unlines $ studStr xs) )xs) list2
+				return()
+
+				
+main = do 
+		[file,avg,co,gr,count,crt] <- getArgs
+		if avg == "avg" then (studAvgAge file (read(co)::Integer) (read(gr)::Integer)) else return()
+		if count == "count" then countStud file else return()
+		if crt == "crt" then allStud file else return()
+		return()
